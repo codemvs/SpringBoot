d2ae4a34a5ed9ee44e61656d689e839b7956bdae
Git MCS <mv.salvador93@gmail.com>
2018-08-12 21:23:42 -0500

    Add repository

---
+++ spring-boot-bootstrap/src/main/java/com/codemvs/app/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package com.codemvs.app.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+	//injectamos valor de property a variable titulo
+	@Value("${application.controller.titulo}")
+	private String titulo;
+	@GetMapping("/index")
+	public String index(Model model) {
+		model.addAttribute("titulo",this.titulo);
+		return "index";		
+	}		
+}
---
+++ spring-boot-thymeleaf/src/main/java/com/codemvs/thymeleaf/com/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.codemvs.thymeleaf.com.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+	@GetMapping("/")
+	public String hola(Model model) {
+		model.addAttribute("mensaje","Hola tymelief Spring");
+		return "hola";
+	}
+}
