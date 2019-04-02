b4a3f532226b0f23724e3a41db61f1adb7e885e3
codemvs <codemvs@gmail.com>
2019-04-02 12:58:26 -0600

    JPA: Consulta JPA para buscar productos en el autocomplete

--- spring-boot-data-jpa/src/main/java/com/codemvs/springboot/app/model/service/ClienteServiceImp.java
+++ spring-boot-data-jpa/src/main/java/com/codemvs/springboot/app/model/service/ClienteServiceImp.java
@@ -9,13 +9,18 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.codemvs.springboot.app.models.dao.IClienteDao;
+import com.codemvs.springboot.app.models.dao.IProductoDao;
 import com.codemvs.springboot.app.models.entity.Cliente;
+import com.codemvs.springboot.app.models.entity.Producto;
 
 @Service
 public class ClienteServiceImp implements IClienteService{
 
 	@Autowired
 	private IClienteDao clienteDao;
+	
+	@Autowired
+	private IProductoDao productoDao;
 	
 	@Override
 	@Transactional(readOnly = true)
@@ -50,6 +55,12 @@ public class ClienteServiceImp implements IClienteService{
 	public Page<Cliente> findAll(Pageable pageable) {
 		// TODO Auto-generated method stub
 		return clienteDao.findAll(pageable);
+	}
+
+	@Override
+	public List<Producto> findByNombre(String term) {
+		// TODO Auto-generated method stub
+		return productoDao.findByNombreLikeIgnoreCase("%"+term+"%");
 	}
 
 
